daf43c1c9089b732bb634a64caf932ec83821f66
Benedito Paiva Brasil Filho <paivarobin16@gmail.com>
2026-02-06 11:28:01 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -12,5 +12,20 @@ export default function Home() {
     startGame(canvas)
   }, [])
 
-  return <canvas ref={canvasRef} />
+  return (
+    <>
+      <div id="hud">
+        <div>Distância: <span id="dist">0</span>m</div>
+        <div>Moedas: <span id="coins">0</span></div>
+      </div>
+
+      <canvas ref={canvasRef} />
+
+      <div id="gameOver">
+        <h2>💥 FIM DE JOGO</h2>
+        <p id="finalScore"></p>
+        <button onClick={() => location.reload()}>Jogar novamente</button>
+      </div>
+    </>
+  )
 }
